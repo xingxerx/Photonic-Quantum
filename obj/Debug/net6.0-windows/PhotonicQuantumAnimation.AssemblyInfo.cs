878f4df3fff059f298284987eb4037840805c689
xing <xingxerx@gmail.com>
2025-09-20 18:34:56 -0400

    Update binaries, assembly information, and source link for PhotonicQuantumAnimation

--- obj/Debug/net6.0-windows/PhotonicQuantumAnimation.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/PhotonicQuantumAnimation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhotonicQuantumAnimation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+144a27763fa68e95f042cf3096b99cbb7ce4c3bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb8e4bd0f0e10083d2162f6b6a6beaf3783f9b53")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhotonicQuantumAnimation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhotonicQuantumAnimation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
