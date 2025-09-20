6c38c75ef765a5923ca195a910556146911fa3f6
xingxerx <xingxerx@gmail.com>
2025-09-20 15:11:44 -0400

    Update assembly informational version and regenerate binaries

--- obj/Debug/net6.0-windows/PhotonicQuantumAnimation.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/PhotonicQuantumAnimation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhotonicQuantumAnimation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e25bd2a7baa9fde6c2540f43ed99d646be5f644")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+699f7bf06ac6b5bf35ffe5df7910e83ae814318a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhotonicQuantumAnimation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhotonicQuantumAnimation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
